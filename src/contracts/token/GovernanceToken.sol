1bf1d96fb507f83029b40387b5ef25d47c36bb9a
Gitter-block <aaron.ong@zoho.com>
2024-12-26 10:16:08 +0800

    Update GovernanceToken.sol

--- src/contracts/token/GovernanceToken.sol
+++ src/contracts/token/GovernanceToken.sol
@@ -1,1 +1,17 @@
+// contracts/token/GovernanceToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernanceToken is ERC20Votes {
+    constructor() 
+        ERC20("DAOGovernance", "DAOG")
+        ERC20Permit("DAOGovernance")
+    {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
 
